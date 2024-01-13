123ead5a391aceef3066e5a8ef96e7aad1d1b657
rrhiemstar <rrhiemstar@gmail.com>
2024-01-13 15:25:09 +0100

    Added dependencies.

--- Project.lua
+++ Project.lua
@@ -3,6 +3,8 @@ Project = {
     uuid = "fb04da88-e7da-4783-a160-8cb18c3a0ab4",
     authors = {"rrhiemstar<rrhiemstar@gmail.com>"},
     version = "0.1.0",
-    deps = {}
+    deps = {
+        DepDep = "0.1.0"
+    }
 }
 return Project
