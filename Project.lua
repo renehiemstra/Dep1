c13126c923f740099d6874ce6fee4c5ff8b289e5
rrhiemstar <rrhiemstar@gmail.com>
2024-01-13 10:26:48 +0100

    <new package> Dep1

---
+++ Project.lua
@@ -0,0 +1,8 @@
+Project = {
+    name = "Dep1",
+    uuid = "fb04da88-e7da-4783-a160-8cb18c3a0ab4",
+    authors = {"rrhiemstar<rrhiemstar@gmail.com>"},
+    version = "0.1.0",
+    deps = {}
+}
+return Project
